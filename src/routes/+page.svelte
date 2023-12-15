d93184fbe4806db8e6b1a2589e5fb869953ebfdc
Adam Fabicki <adam@fabicki.com>
2023-12-15 16:35:39 -0500

    header with edit button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,31 @@
     import {title, wall} from "$lib/wall.json"
 </script>
 
-<div class="header">
+<style>
+    header {
+        height: 30px;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+
+        background-color: rgb(48, 95, 40);
+        padding: 5px;
+        margin: 0px;
+    }
+
+    .title {
+        color: white;
+    }
+</style>
+
+<header>
     <div class="title">{title}</div>
-    <Switch size="lg" offLabel="view" onLabel="edit" />
-</div>
+    <div class="edit-switch">
+        <Switch size="lg" offLabel="view" onLabel="edit" />
+    </div>
+</header>
 
 <div>
     {#each wall as sticker}
